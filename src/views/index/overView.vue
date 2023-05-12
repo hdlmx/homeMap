2926824199661b848e768aebc9c82d694b9d5c6e
hdlmx <hdlmx@outlook.com>
2023-05-12 09:38:28 +0000

    echart

---
+++ src/views/index/overView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+
+</script>
+<template>
+  <div>
+    <el-row>
+      <el-col :span="6">出行</el-col>
+      <el-col :span="6">生活缴费</el-col>
+      <el-col :span="6">收入</el-col>
+      <el-col :span="6">支出</el-col>
+    </el-row>
+  </div>
+
+</template>
+<style scoped>
+
+</style>
---
+++ src/views/indexDashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="dashPanel">
+    <el-row style="width: 100%">
+      <el-col :span="24">
+        <overview>
+        </overview>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <car></car>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24"></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup lang="ts">
+import overview from "@/views/index/overView.vue";
+import car from "@/views/index/car.vue";
+</script>
+
+<style scoped>
+.dashPanel {
+  display: grid;
+  width: 100%;
+  grid-template-rows: 1fr 3fr 3fr;
+
+}
+</style>
